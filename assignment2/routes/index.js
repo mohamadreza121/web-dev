1ad6b6fa6ae6f0e0af999c2ddf18f894c31920d4
mohamadreza121 <mrheidarpoor7@gmail.com>
2024-10-31 20:50:38 -0400

    added content
    added content to my home.ejs, about.ejs, and contact.ejs
    
    also added image, pdf and video to public folder

--- assignment2/routes/index.js
+++ assignment2/routes/index.js
@@ -2,23 +2,21 @@ let express = require('express');
 let router = express.Router();
 
 // Home page
-router.get('/', (req, res) => {
-  res.render('home', { title: 'Home' });
-});
-
-// Existing routes
 router.get('/home', (req, res) => {
   res.render('home', { title: 'Home' });
 });
 
+// About page
 router.get('/about', (req, res) => {
   res.render('about', { title: 'About Us' });
 });
 
+// Projects page
 router.get('/projects', (req, res) => {
   res.render('projects', { title: 'Projects' });
 });
 
+// Contact page
 router.get('/contact', (req, res) => {
   res.render('contact', { title: 'Contact Us' });
 });
